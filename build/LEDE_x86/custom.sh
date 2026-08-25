1e73673f3c258ee74228abe84a6a72cc8d9e0bc8
小橙子 <615760954@qq.com>
2026-08-25 23:12:46 +0800

    修复 luci-theme-argon 的克隆链接格式

--- build/LEDE_x86/custom.sh
+++ build/LEDE_x86/custom.sh
@@ -14,7 +14,7 @@ cat feeds.conf.default
 
 git clone --depth 1 -b 18.06 https://github.com/xiaozhuai/luci-app-filebrowser package/luci-app-filebrowser
 git clone --depth 1 https://github.com/gdy666/luci-app-lucky.git package/lucky
-git clone --depth 1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone --depth 1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone --depth 1 https://github.com/vernesong/OpenClash.git package/openclash && mv package/openclash/luci-app-openclash package/ && rm -rf package/openclash
 git clone --depth 1 https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
 
