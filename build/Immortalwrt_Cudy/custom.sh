59d1cc47ecc44190e465bf1c14fc4c1dbb3949df
小橙子 <615760954@qq.com>
2026-09-10 22:43:30 +0800

    修复 root 密码设置方式，确保密码正确更新

--- build/Immortalwrt_Cudy/custom.sh
+++ build/Immortalwrt_Cudy/custom.sh
@@ -35,7 +35,7 @@ uci set system.@system[0].hostname='CudyTR3000'
 uci set luci.main.mediaurlbase=/luci-static/argon
 uci set network.lan.ipaddr='192.168.10.1'
 uci set network.lan.netmask='255.255.255.0'
-echo "root:zybin980329" | chpasswd
+echo -e "zybin980329\nzybin980329" | passwd root
 
 uci commit
 EOF
