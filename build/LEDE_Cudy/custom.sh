b4e675c55223a8f5742cf64f17f28b653e8cc0de
小橙子 <67323819+xcz-ns@users.noreply.github.com>
2026-09-22 17:58:38 +0800

    更新 custom.sh

--- build/LEDE_Cudy/custom.sh
+++ build/LEDE_Cudy/custom.sh
@@ -15,7 +15,7 @@ cat feeds.conf.default
 git clone --depth 1 https://github.com/kenzok8/small-package small-package
 cp -rf small-package/{luci-app-ramfree,luci-app-poweroff} package/
 
-git clone --depth 1 https://github.com/animegasan/luci-app-wolplus package/luci-app-wolplus
+git clone --depth 1 https://github.com/xcz-ns/luci-app-wolplus package/luci-app-wolplus
 git clone --depth 1 https://github.com/gdy666/luci-app-lucky.git package/lucky
 git clone --depth 1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone --depth 1 https://github.com/vernesong/OpenClash.git package/openclash \
